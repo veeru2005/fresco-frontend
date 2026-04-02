606027f5b614d237ec07b72fcbe0c9981fcecfb7
veeru2005 <sunkavalli.veerendra1973@gmail.com>
2026-04-02 15:14:29 +0530

    frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
+import viteCompression from 'vite-plugin-compression';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({ 
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1024 
+    }),
+    viteCompression({ 
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024 
+    })
+  ],
+  build: {
+    target: 'esnext',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
